741a38b6dd52629f0781f8354107e6c4f3f41098
PaoloMucedero <p.mucedero@gmail.com>
2025-12-02 09:53:33 +0100

    Inizio esercizio bonus

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -6,17 +6,17 @@ const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = 0;
+const cityIndex = 4;
 console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = '';
+const citySubstring = 'York';
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = '';
+const cityReplaced = 'Delhi';
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
